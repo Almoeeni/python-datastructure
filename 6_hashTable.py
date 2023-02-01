757b810ca1316529ac62cc41f8f3a6ff134488b6
almoeeni <almoiniefazal@live.co.uk>
2023-02-01 20:36:14 +0500

    adding hash table DSA

---
+++ 6_hashTable.py
@@ -0,0 +1,45 @@
+class HashTable:
+    def __init__(self, size=7) -> None:
+        self.data_map = [None] * size
+
+    def __hash(self, key):
+        my_hash = 0
+        for letter in key:
+            my_hash = (my_hash + ord(letter) * 23) % len(self.data_map)
+        return my_hash
+
+    def print_table(self):
+        for i, val in enumerate(self.data_map):
+            print(i, ":", val)
+
+    def set_item(self, key, value):
+        index = self.__hash(key)
+        if self.data_map[index] == None:
+            self.data_map[index] = []
+        self.data_map[index].append([key, value])
+
+    def get_item(self, key):
+        index = self.__hash(key)
+        if self.data_map[index] is not None:
+            for i in range(len(self.data_map[index])):
+                if self.data_map[index][i][0] == key:
+                    return self.data_map[index][i]
+        return None
+
+    def get_key(self):
+        _keys = []
+        for i in range(len(self.data_map)):
+            if self.data_map[i] is not None:
+                for j in range(len(self.data_map[i])):
+                    _keys.append(self.data_map[i][j][0])
+        return _keys
+
+
+my_hash_table = HashTable()
+
+
+my_hash_table.set_item("bolts", 1400)
+my_hash_table.set_item('washers', 50)
+my_hash_table.set_item('lumber', 70)
+my_hash_table.print_table()
+print(my_hash_table.get_key())
